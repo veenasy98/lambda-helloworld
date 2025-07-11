a1456a178c28073b4fd62b5827512bab39f8c488
veenasy98 <veena.yaraddi.ext@bayer.com>
2025-07-11 16:34:05 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,6 +2,7 @@ output "lambda_function_name" {
   value = aws_lambda_function.lambda.function_name
 }
 
-output "api_gateway_url" {
-  value = aws_api_gateway_deployment.deployment.invoke_url
+output "api_gateway_invoke_url" {
+  description = "Invoke URL of the API Gateway"
+  value       = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}/lambda"
 }
